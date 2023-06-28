1ccbacc8bac9dd0ee4d9984bddfabd82a6aedfa6
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-06-28 16:50:06 +0900

    123

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 def example_view(request):
     # my_app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+
+def variable_view(request):
+    
+    
+    
+    my_var = {'first_name': 'John','last_name': 'Doe',  # 딕셔너리 형태로 변수를 만들어서 템플릿에 전달
+              'some_list':[1,2,3],  'some_dict':{'inside_key':'inside_value'} } 
+    
+    return render(request, 'my_app/variable.html', context=my_var)  # (요청 객체, 템플릿 파일, 딕셔너리 형태의 변수)
