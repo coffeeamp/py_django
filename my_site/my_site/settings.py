3c308afef96395239e6ef38a1170e8b3bc3d508c
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-07-30 22:51:19 +0900

    cors_setting_진행중

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'my_site.urls'
