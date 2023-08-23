cd27bb918eff8dca6f9175d2af5108b770092f05
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-08-23 23:54:57 +0900

    post_todo

--- my_site/my_app/models.py
+++ my_site/my_app/models.py
@@ -58,3 +58,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+#----------------------------------------------#
+# todo Test
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
